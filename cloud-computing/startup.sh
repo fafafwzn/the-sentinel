9f7ad9198ed175c504370657c0799f77b02d2b9d
Hazunanafaru <hazunanafaru@gmail.com>
2021-07-16 14:14:40 +0700

    Update main.py (change the model directory) and startup.sh (remove virtual env)

--- cloud-computing/startup.sh
+++ cloud-computing/startup.sh
@@ -1,6 +1,5 @@
 #! /usr/bin/bash
+sudo apt install unzip
 sudo pip3 install virtualenv
-virtualenv the-sentinel
-source ~/the-sentinel/bin/activate
 sudo pip3 install -r requirements.txt --no-cache-dir
 gdown https://drive.google.com/uc?id=1QYh8lid_u052BKXMxKiHrX1TVvsEso11
