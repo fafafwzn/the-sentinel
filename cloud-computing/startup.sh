265ce3e978b588fc64ffde95c8cf87ec6b6657bc
Hazunanafaru <hazunanafaru@gmail.com>
2021-07-16 13:32:20 +0700

    Update startup.sh

--- cloud-computing/startup.sh
+++ cloud-computing/startup.sh
@@ -1,7 +1,4 @@
 #! /usr/bin/bash
-sudo yum git -y
-git clone https://github.com/fafafwzn/the-sentinel.git
-cd the-sentinel/cloud-computing
 pip3 install gdown
 pip3 install -r requirements.txt
 gdown https://drive.google.com/uc?id=1QYh8lid_u052BKXMxKiHrX1TVvsEso11
